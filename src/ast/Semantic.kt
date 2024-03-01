73d3eb8afcceb5136386212de85f5a59e3fb56cf
Will <47335043+williamfedele@users.noreply.github.com>
2024-03-01 17:47:21 -0500

    example1.src complete

--- src/ast/Semantic.kt
+++ src/ast/Semantic.kt
@@ -10,8 +10,17 @@ class Semantic {
             "A5" to Triple("makeFamilyUntilNull", "Prog", -1),
             "A6" to Triple("makeFamilyUntilNull", "InheritList", -1),
             "A7" to Triple("makeFamilyUntilNull", "Struct", -1),
+            "A8" to Triple("makeFamilyUntilNull", "FuncParamList", -1),
+            "A9" to Triple("makeFamily", "FuncHead", 3),
+            "A10" to Triple("makeFamily", "AssignStat", 2),
+            "A11" to Triple("makeFamilyUntilNull", "FuncBody", -1),
+            "A12" to Triple("makeFamily", "AddOp", 2),
+            "A13" to Triple("makeFamily", "MultOp", 2),
             "T" to Triple("makeNode", "Type", -1),
             "E" to Triple("makeNull", "", -1),
+            "M" to Triple("makeNode", "MultOp", -1),
+
+
         )
 
     }
