79b493d48acf38b51c56b6a20282e9d702c8785e
Will <47335043+williamfedele@users.noreply.github.com>
2024-03-08 17:13:55 -0500

    Node ordering corrected. Siblings+leftmostChild

--- src/ast/AST.kt
+++ src/ast/AST.kt
@@ -3,9 +3,11 @@ package ast
 import lexer.Token
 import parser.Stack
 import parser.pop
+import parser.push
 import parser.top
 import java.io.File
 import java.util.*
+
 
 /**
  * Contains operations for manipulating a semantic stack
@@ -16,23 +18,36 @@ class AST {
         fun makeNode(t: Token, s: String): Node{
             return Node(t.type.repr.uppercase(Locale.getDefault()), null, t)
         }
-        // TODO: give children nodes connections to their right sibling and the first child.
-        // give parent connection to first child.
+
         fun makeFamilyUntilNull(sstack: Stack<Node?>, parent: String): Node {
             val new = Node(parent)
+            val inOrderStack = Stack<Node?>() // put nodes in order due to reversed nature of stack
+
             while (sstack.top() != null) {
-                new.addChild(sstack.pop()!!)
+                inOrderStack.push(sstack.pop()!!)
             }
             sstack.pop()
+
+            while (inOrderStack.isNotEmpty()) {
+                new.addChild(inOrderStack.pop()!!)
+            }
+
             return new
         }
 
         // Makes the n nodes on top of the stack children of a new node created with the parent string
         fun makeFamily(sstack: Stack<Node?>, parent: String, n: Int): Node {
             val new = Node(parent)
+            val inOrderStack = Stack<Node?>() // put nodes in order due to reversed nature of stack
+
             for (i in 1..n) {
-                new.addChild(sstack.pop()!!)
+                inOrderStack.push(sstack.pop()!!)
             }
+
+            while (inOrderStack.isNotEmpty()) {
+                new.addChild(inOrderStack.pop()!!)
+            }
+
             return new
         }
 
@@ -53,7 +68,7 @@ class AST {
             outputFile.appendText("$padding${root.name}\n")
 
             //println("$padding${root.name}")
-            for (child: Node in root.children.reversed()) {
+            for (child: Node in root.children) {
                 astPrint(child, "$padding| ", outputFile)
             }
         }
--- src/ast/Node.kt
+++ src/ast/Node.kt
@@ -6,9 +6,23 @@ data class Node(val s: String, var parent: Node? = null, var t: Token? = null) {
 
     val name = s
     val children = mutableListOf<Node>()
-    var leftmostChild = null
+    var leftmostChild: Node? = null
+    var leftmostSibling: Node? = null
+    var rightSibling: Node? = null
 
+    // Adding a child node to a parent
     fun addChild(child: Node) {
+        if (children.isEmpty()) {
+            // parent has no children, create a pointer to the new child as leftmostChild
+            leftmostChild = child
+        }
+        else {
+            // parent has children, add the new child as a rightSibling to the rightmost sibling
+            children.last().rightSibling = child
+            // link the new child to its leftmostSibling, i.e. the leftmostChild of 'this' Node (parent)
+            child.leftmostSibling = leftmostChild
+        }
+
         children.add(child)
         child.parent = this
     }
